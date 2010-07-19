280175453527d172c3635c44c071f720b2776e3f
Atomic <julien.schmidt@wanadoo.fr>
2010-07-20 01:05:43 +0200

    Pret a tirer ( ID : 56342 ).

---
+++ addition/Sorts/06_mangos_spell_proc_event.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_proc_event WHERE entry = '56342';
+INSERT INTO `spell_proc_event` VALUES ('56342', '0', '9', '12', '134217728', '393216', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '22');
