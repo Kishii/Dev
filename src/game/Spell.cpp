39f3c6b067d8c5d2f46b5d1c2a45898278fa363f
Atomic <julien.schmidt@wanadoo.fr>
2010-07-20 05:47:56 +0200

    Matrise des runes de la mort, Moisson et Sang du Nord

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4124,7 +4124,10 @@ SpellCastResult Spell::CheckOrTakeRunePower(bool take)
                 --runeCost[rune];
 
                 if (take)
+                {
                     plr->ConvertRune(i, plr->GetBaseRune(i));
+                    plr->ClearConvertedBy(i);
+                }
             }
         }
 
