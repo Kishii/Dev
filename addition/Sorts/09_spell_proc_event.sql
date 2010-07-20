6c75679da997fa758a9232ee3dc400766b8911ad
Atomic <julien.schmidt@wanadoo.fr>
2010-07-20 03:33:13 +0200

    Colere dechainee ( ID : 12322 ).

---
+++ addition/Sorts/09_spell_proc_event.sql
@@ -0,0 +1,5 @@
+UPDATE `spell_proc_event` SET `ppmRate` = 3 WHERE `entry` = 12322;
+UPDATE `spell_proc_event` SET `ppmRate` = 6 WHERE `entry` = 12999;
+UPDATE `spell_proc_event` SET `ppmRate` = 9 WHERE `entry` = 13000;
+UPDATE `spell_proc_event` SET `ppmRate` = 12 WHERE `entry` = 13001;
+UPDATE `spell_proc_event` SET `ppmRate` = 15 WHERE `entry` = 13002;
