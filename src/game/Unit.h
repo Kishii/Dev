90bc1161382dc66db29f65ec89345dfa4dfc078c
Atomic <julien.schmidt@wanadoo.fr>
2010-07-20 02:27:11 +0200

    Doigts de givre ( ID : 44543 ).

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1381,6 +1381,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         bool IsPolymorphed() const;
 
         bool isFrozen() const;
+		bool isIgnoreUnitState(SpellEntry const *spell);
 
         void RemoveSpellbyDamageTaken(AuraType auraType, uint32 damage);
 
@@ -1932,6 +1933,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         uint32 m_reactiveTimer[MAX_REACTIVE];
         uint32 m_regenTimer;
         uint32 m_lastManaUseTimer;
+		float m_lastAuraProcRoll;
 
     private:
         void CleanupDeletedAuras();
