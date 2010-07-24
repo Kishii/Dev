0d3ca6de1d77dba5486150c1542c3de4f63c41a1
Atomic <julien.schmidt@wanadoo.fr>
2010-07-24 18:08:45 +0200

    Suppression du commit 5bcb6c16be9c12a8cb02df18f38a0113f32247f4

--- sql/updates/10244_01_mangos_command.sql
+++ sql/updates/10244_01_mangos_command.sql
@@ -2,4 +2,4 @@ ALTER TABLE db_version CHANGE COLUMN required_10237_01_mangos_spell_bonus_data r
 
 DELETE FROM command WHERE name = 'stable';
 INSERT INTO command (name, security, help) VALUES
-('stable',3,'Syntax: .stable\r\n\r\nShow your pet stable.');
+('stable',0,'Syntax: .stable\r\n\r\nShow your pet stable.');
