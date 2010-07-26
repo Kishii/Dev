839646546d052dbe425f9d9a1c2eeea79e99c1b3
Atomic <julien.schmidt@wanadoo.fr>
2010-07-27 00:21:54 +0200

    Fix penitence

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2127,6 +2127,20 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 }
                 break;
             }
+            case SPELLFAMILY_PRIEST:
+            {
+                // Penance
+                if (m_spellProto->SpellIconID == 225)
+                {
+                    Unit* caster = GetCaster();
+                    if (!caster || !target || caster->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    ((Player*)caster)->SetSelection(target->GetGUID());
+                    return;
+                }
+                break;
+            }
             case SPELLFAMILY_SHAMAN:
             {
                 // Tidal Force
