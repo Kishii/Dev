c92a125431c37a044596ceb8e68480fd1fcc6ac2
Atomic <julien.schmidt@wanadoo.fr>
2010-07-27 14:34:55 +0200

    Fix SQL

--- addition/05_mangos_name_announce.sql
+++ addition/05_mangos_name_announce.sql
@@ -1,9 +1,9 @@
 INSERT INTO `command` VALUES ('nameannounce',6,'Syntax: .nameannounce $MessageToBroadcast\r\n\r\nSend a global message to all players online in chat log with colored sender\'s name.');
-INSERT INTO `mangos_string` VALUES ('65', '|cffffffff [ Joueur ( Aucun ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('66', '|cffffffff [ Joueur ( Bronze ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('67', '|cffffffff [ Joueur ( Argent ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('68', '|cffffffff [ Joueur ( Or ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('69', '|cffffffff [ Joueur ( Prenium ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('70', '|cffffffff [ Joueur ( Cataclysm ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('71', '|cffffffff [ Maître de jeu : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
-INSERT INTO `mangos_string` VALUES ('72', '|cffffffff [ Administrateur : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('65', '|cffffffff[ Joueur ( Aucun ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('66', '|cffffffff[ Joueur ( Bronze ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('67', '|cffffffff[ Joueur ( Argent ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('68', '|cffffffff[ Joueur ( Or ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('69', '|cffffffff[ Joueur ( Prenium ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('70', '|cffffffff[ Joueur ( Cataclysm ) : %s ] :|CFFf1a400 %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('71', '|cffffffff[ Maître de jeu : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('72', '|cffffffff[ Administrateur : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
