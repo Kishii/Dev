7287b9fe329cf1c5502b744dc15d13a616996a0d
Atomic <julien.schmidt@wanadoo.fr>
2010-08-18 07:19:30 +0200

    +/- Nettoyage SQL

--- sql/GMISLAND/03_Jugement_de_lumiere.sql
+++ sql/GMISLAND/03_Jugement_de_lumiere.sql
@@ -1,5 +1,5 @@
 /*Judgement of Light & Judgement of Wisdom spell proc fix*/
 DELETE FROM `spell_proc_event` WHERE `entry` IN(20185, 20186);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMaskA0`, `SpellFamilyMaskA1`, `SpellFamilyMaskA2`, `SpellFamilyMaskB0`, `SpellFamilyMaskB1`, `SpellFamilyMaskB2`, `SpellFamilyMaskC0`, `SpellFamilyMaskC1`, `SpellFamilyMaskC2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(20185, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 50, 0),
-(20186, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 50, 0);
+(20185, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 50, 0),
+(20186, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 50, 0);
